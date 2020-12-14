308191097f13690b76796e492ada48a7bdae56ca
Leonardo Santos <leolmblsantos@gmail.com>
2020-12-14 15:49:10 -0300

    Projeto criado

---
+++ src/test/java/com/projetoautomacao/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoautomacao.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
