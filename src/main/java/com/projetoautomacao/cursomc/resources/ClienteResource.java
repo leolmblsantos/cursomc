52d50e3fd2443ac61d1a57ac6622d218b9a33c83
Leonardo Santos <leolmblsantos@gmail.com>
2020-12-16 08:27:20 -0300

    Correcoes: update em ctegoriasService e construtor de pagamento

--- src/main/java/com/projetoautomacao/cursomc/resources/ClienteResource.java
+++ src/main/java/com/projetoautomacao/cursomc/resources/ClienteResource.java
@@ -1,6 +1,5 @@
 package com.projetoautomacao.cursomc.resources;
 
-import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,9 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.projetoautomacao.cursomc.domain.Cliente;
 import com.projetoautomacao.cursomc.domain.Cliente;
 import com.projetoautomacao.cursomc.dto.ClienteDTO;
 import com.projetoautomacao.cursomc.service.ClienteService;
