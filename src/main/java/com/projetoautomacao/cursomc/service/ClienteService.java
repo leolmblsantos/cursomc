35608976702b1ce42a6f1564951b0acd8b61600a
Leonardo Santos <leolmblsantos@gmail.com>
2020-12-16 14:40:31 -0300

    Delecao de cliente, validacao customizada de CPF ou CNPJ na insercao de cliente

--- src/main/java/com/projetoautomacao/cursomc/service/ClienteService.java
+++ src/main/java/com/projetoautomacao/cursomc/service/ClienteService.java
@@ -58,7 +58,7 @@ public class ClienteService {
 		try {
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityViolationException("Não é possível excluir porque há entidades relacionadas");
+			throw new DataIntegrityViolationException("Não é possível excluir porque há pedidos relacionadas");
 		}
 	}
 	
