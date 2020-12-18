33ac73749fef617a91d2b433290b01e52be78663
Leonardo Santos <leolmblsantos@gmail.com>
2020-12-18 13:49:54 -0300

    Autorizando endpoints para perfis especificos

--- src/main/java/com/projetoautomacao/cursomc/security/JWTAuthorizationFilter.java
+++ src/main/java/com/projetoautomacao/cursomc/security/JWTAuthorizationFilter.java
@@ -36,7 +36,7 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
 		String header = request.getHeader("Authorization");
 		
 		if (header !=null && header.startsWith("Bearer ")) {
-			UsernamePasswordAuthenticationToken auth = getAuthentication(request, header.substring(7));
+			UsernamePasswordAuthenticationToken auth = getAuthentication(header.substring(7));
 			if (auth !=null ) {
 				SecurityContextHolder.getContext().setAuthentication(auth);
 			}
@@ -44,7 +44,7 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
 		chain.doFilter(request, response);
 	}
 
-	private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request, String token) {
+	private UsernamePasswordAuthenticationToken getAuthentication(String token) {
 		if (jwtUtil.tokenValido(token)) {
 			String username = jwtUtil.getUsername(token);
 			UserDetails user = userDetailsService.loadUserByUsername(username);
